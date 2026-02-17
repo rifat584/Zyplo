fa67c299d307acc55b0b5b749e117f46d5ded2ed
ebrahim2355 <web.ebrahimali@gmail.com>
2026-02-17 11:28:39 +0600

    feat: folder stucture solved, add animated hero section and UI mock

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,15 +1,11 @@
-import Navbar from "@/components/Navbar/Navbar"; 
+import Navbar from "@/components/layout/Navbar/Navbar";
 import "./globals.css";
-import WorkflowStepper from "@/components/WorkFlow/WorkflowStepper";
-import TimeTracking from "@/components/TimeTracking/TimeTracking";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <Navbar />
-        <WorkflowStepper/>
-        <TimeTracking/>
         {children}
       </body>
     </html>
