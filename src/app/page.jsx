8df2a07768ad9461723ac5c1d8c1b29221f27a82
ebrahim2355 <web.ebrahimali@gmail.com>
2026-02-17 11:28:39 +0600

    feat: folder stucture solved, add animated hero section and UI mock

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,13 @@
-import Navbar from '@/components/Navbar/Navbar';
+import Hero from '@/components/Home/Hero/Hero';
+import TimeTracking from '@/components/Home/TimeTracking/TimeTracking';
+import WorkflowStepper from '@/components/Home/WorkFlow/WorkflowStepper';
 
 export default function Home() {
   return (
     <main>
-      
+      <Hero />
+      <WorkflowStepper />
+      <TimeTracking />
     </main>
   );
 }
