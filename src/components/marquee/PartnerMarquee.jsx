f59d3da38d8b31044d7bdb7bdf94ac22b5897c60
rifat584 <nowucme34@gmail.com>
2026-02-17 10:16:45 +0600

    Project Started By Rifat

---
+++ src/components/marquee/PartnerMarquee.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PartnerMarquee = () => {
+  return (
+    <div>
+      <h3>Our Partner</h3>
+      <p>Marquee Goes Here</p>
+    </div>
+  );
+};
+
+export default PartnerMarquee;
